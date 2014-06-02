71ad19fa038816da47a62cc1f43ed06fd824dc4f
auy555555 <EFrourperly6856@cuvox.de>
2014-06-02 12:07:41 +0800

    update

--- javascripts/_vti_cnf/main.js
+++ javascripts/_vti_cnf/main.js
@@ -3,4 +3,4 @@ vti_timelastmodified:TR|02 Jun 2014 03:45:46 -0000
 vti_extenderversion:SR|6.0.2.8161
 vti_cacheddtm:TX|02 Jun 2014 03:45:46 -0000
 vti_filesize:IR|49
-vti_backlinkinfo:VX|
+vti_backlinkinfo:VX|index.html
