6d35753ebc2d501665f8037c4bd338550dd637b4
auy555555 <EFrourperly6856@cuvox.de>
2014-06-02 12:13:43 +0800

    update

--- javascripts/_vti_cnf/main.js
+++ javascripts/_vti_cnf/main.js
@@ -1,10 +1,10 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|02 Jun 2014 04:10:06 -0000
+vti_timelastmodified:TR|02 Jun 2014 04:12:06 -0000
 vti_extenderversion:SR|6.0.2.8161
 vti_backlinkinfo:VX|index.html
 vti_author:SR|C6KQFXPBZEERGAN\\Administrator
 vti_modifiedby:SR|C6KQFXPBZEERGAN\\Administrator
-vti_nexttolasttimemodified:TW|02 Jun 2014 03:45:46 -0000
+vti_nexttolasttimemodified:TW|02 Jun 2014 04:10:06 -0000
 vti_timecreated:TR|02 Jun 2014 04:10:06 -0000
-vti_cacheddtm:TX|02 Jun 2014 04:10:06 -0000
-vti_filesize:IR|65
+vti_cacheddtm:TX|02 Jun 2014 04:12:06 -0000
+vti_filesize:IR|49
